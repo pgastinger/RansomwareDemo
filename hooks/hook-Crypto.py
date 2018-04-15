faf18b1df87f7c06d7758aa9ddfacb265935bea9
Peter Gastinger <peter.gastinger+github@gmail.com>
2018-04-15 10:58:34 +0200

    Create hook-Crypto.py

---
+++ hooks/hook-Crypto.py
@@ -0,0 +1,9 @@
+# Hook for pycryptodome extensions
+
+hiddenimports = [
+ 'Crypto.Cipher._raw_aes',
+ 'Crypto.Cipher._raw_ctr',
+ 'Crypto.Hash._SHA256',
+ 'Crypto.Util._cpuid',
+ 'Crypto.Util._strxor',
+]
